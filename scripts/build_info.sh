1ec76db43d3a774854a8a2edb2eac8994fcce9fd
Thomas Refis <thomas.refis@gmail.com>
2017-06-28 11:34:24 +0100

    v0.9.115.06+43

--- scripts/build_info.sh
+++ scripts/build_info.sh
@@ -5,4 +5,4 @@ set -e -o pipefail
 
 username=$(whoami 2>/dev/null || echo "user-id-$(id -u)")
 
-echo -n "((username $username) (hostname $(hostname)) (kernel $(uname -r)) (build_date $(date -u +%Y-%m-%d)) (build_time $(date -u +%H:%M:%S)) $1)"
+echo -n "((username $username) (hostname $(hostname)) (kernel $(uname -r)) (build_time \"$(date -u "+%Y-%m-%d %H:%M:%S")Z\") $1)"
